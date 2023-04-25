3a26ec872c7e2c89c1c2ead29629107ac6318be5
Luka Krševan Nižić <lnizic21@foi.hr>
2023-04-25 16:20:00 +0200

    Promjene init

--- EvaluationMenagerG10/FrmLogin.cs
+++ EvaluationMenagerG10/FrmLogin.cs
@@ -10,11 +10,15 @@ using System.Windows.Forms;
 
 namespace EvaluationMenagerG10
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
         }
     }
 }
